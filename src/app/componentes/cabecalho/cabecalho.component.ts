28a89e897821f48a3efb5997f18c2511e4392f70
Gustavo-Oliveira-Brandao <gustavooliveirabrandao2021@gmail.com>
2023-12-18 00:11:58 -0300

    Adição de inputs no cabeçalho

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cabecalho',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cabecalho.component.html',
   styleUrl: './cabecalho.component.css'
 })
 export class CabecalhoComponent {
+  raca = {
+    id: 'raca',
+    valor: ''
+  }
+
+  classe = {
+    id: 'classe',
+    valor: ''
+  }
+
+  origem = {
+    id: 'origem',
+    valor: ''
+  }
+
+  nivel = {
+    id: 'nivel',
+    valor: 0
+  }
+
+  divindade = {
+    id: 'divindade',
+    valor: ''
+  }
 
 }
