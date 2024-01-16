a72fa0531123e59ca49ed7c9f0f35280f37d4afc
Gustavo-Oliveira-Brandao <gustavooliveirabrandao2021@gmail.com>
2024-01-16 15:57:48 -0300

    Backup

--- src/app/componentes/ficha/ficha.component.ts
+++ src/app/componentes/ficha/ficha.component.ts
@@ -44,12 +44,15 @@ export class FichaComponent {
     modificadorCarisma: 0,
   };
 
-  acrobacia = {
-    id: 'acrobacia',
-    atributo: 0,
-    grau: 'D',
-    valorGrau: 0,
+  pericia = {
+    acrobacia: {
+      id: 'acrobacia',
+      atributo: 0,
+      grau: 'D',
+      valorGrau: 0,
+    },
   };
+  acrobacia = {};
 
   arcanismo = {
     id: 'arcanismo',
@@ -231,67 +234,99 @@ export class FichaComponent {
     atributo: 10,
   };
 
+  d20 = {
+    id: 'd20',
+    sides: 20,
+  };
+
   atualizarAtributos() {
     this.forca.modificadorForca = Math.trunc((this.forca.atributo - 10) / 2);
+
     this.destreza.modificadorDestreza = Math.trunc(
       (this.destreza.atributo - 10) / 2
     );
+
     this.constituicao.modificadorConstituicao = Math.trunc(
       (this.constituicao.atributo - 10) / 2
     );
+
     this.inteligencia.modificadorInteligencia = Math.trunc(
       (this.inteligencia.atributo - 10) / 2
     );
+
     this.sabedoria.modificadorSabedoria = Math.trunc(
       (this.sabedoria.atributo - 10) / 2
     );
+
     this.carisma.modificadorCarisma = Math.trunc(
       (this.carisma.atributo - 10) / 2
     );
 
     this.acrobacia.atributo =
       this.destreza.modificadorDestreza + this.acrobacia.valorGrau;
+
     this.arcanismo.atributo =
       this.inteligencia.modificadorInteligencia + this.arcanismo.valorGrau;
+
     this.atletismo.atributo =
       this.forca.modificadorForca + this.atletismo.valorGrau;
+
     this.cavalgar.atributo =
       this.destreza.modificadorDestreza + this.cavalgar.valorGrau;
+
     this.diplomacia.atributo =
       this.carisma.modificadorCarisma + this.diplomacia.valorGrau;
+
     this.enganacao.atributo =
       this.carisma.modificadorCarisma + this.enganacao.valorGrau;
+
     this.fortitude.atributo =
       this.constituicao.modificadorConstituicao + this.fortitude.valorGrau;
+
     this.furtividade.atributo =
       this.destreza.modificadorDestreza + this.furtividade.valorGrau;
+
     this.intimidacao.atributo =
       this.carisma.modificadorCarisma + this.intimidacao.valorGrau;
+
     this.ladroagem.atributo =
       this.destreza.modificadorDestreza + this.ladroagem.valorGrau;
+
     this.manufatura.atributo =
       this.inteligencia.modificadorInteligencia + this.manufatura.valorGrau;
+
     this.medicina.atributo =
       this.sabedoria.modificadorSabedoria + this.medicina.valorGrau;
+
     this.natureza.atributo =
       this.sabedoria.modificadorSabedoria + this.natureza.valorGrau;
+
     this.ocultismo.atributo =
       this.inteligencia.modificadorInteligencia + this.ocultismo.valorGrau;
+
     this.percepcao.atributo =
       this.sabedoria.modificadorSabedoria + this.percepcao.valorGrau;
+
     this.perfomance.atributo =
       this.carisma.modificadorCarisma + this.perfomance.valorGrau;
+
     this.reflexos.atributo =
       this.destreza.modificadorDestreza + this.reflexos.valorGrau;
+
     this.religiao.atributo =
       this.sabedoria.modificadorSabedoria + this.religiao.valorGrau;
+
     this.sociedade.atributo =
       this.inteligencia.modificadorInteligencia + this.sociedade.valorGrau;
+
     this.sobrevivencia.atributo =
       this.sabedoria.modificadorSabedoria + this.sobrevivencia.valorGrau;
+
     this.vontade.atributo =
       this.sabedoria.modificadorSabedoria + this.vontade.valorGrau;
   }
+
+  carregarConteudo() {}
 
   selectedValue?: string;
 
@@ -325,8 +360,6 @@ export class FichaComponent {
     this.atletismo.grau = String(this.selectedValue);
     let valor = this.grauTreinamento(this.atletismo.grau);
     this.atletismo.valorGrau = valor;
-    console.log(this.selectedValue);
-    console.log(this.atletismo.valorGrau);
   }
 
   arcanismoTreinamento() {
@@ -454,4 +487,58 @@ export class FichaComponent {
     let valor = this.grauTreinamento(this.vontade.grau);
     this.vontade.valorGrau = valor;
   }
+
+  rolagemD20() {
+    var randomNumber = Math.floor(Math.random() * this.d20.sides) + 1;
+    return randomNumber;
+  }
+
+  acrobaciaRolagem() {
+    let sla = this.rolagemD20();
+    console.log(sla);
+  }
+
+  atletismoRolagem() {}
+
+  arcanismoRolagem() {}
+
+  cavalgarRolagem() {}
+
+  diplomaciaRolagem() {}
+
+  enganacaoRolagem() {}
+
+  fortitudeRolagem() {}
+
+  furtividadeRolagem() {}
+
+  intimidacaoRolagem() {}
+
+  ladroagemRolagem() {}
+
+  manufaturaRolagem() {}
+
+  medicinaRolagem() {}
+
+  naturezaRolagem() {}
+
+  ocultismoRolagem() {}
+
+  percepcaoRolagem() {}
+
+  perfomanceRolagem() {}
+
+  reflexosRolagem() {}
+
+  religiaoRolagem() {}
+
+  saberRolagem() {}
+
+  saber2Rolagem() {}
+
+  sociedadeRolagem() {}
+
+  sobrevivenciaRolagem() {}
+
+  vontadeRolagem() {}
 }
