2404cc46ff2be1705b650cf447bf34f2021885fb
Gustavo-Oliveira-Brandao <gustavooliveirabrandao2021@gmail.com>
2024-02-27 19:26:02 -0300

    Alterações visuais em ações(antigamente conhecido como ataques/magias)

--- src/app/paginas/ficha/componentes/acoes/acoes.component.ts
+++ src/app/paginas/ficha/componentes/acoes/acoes.component.ts
@@ -1,5 +1,5 @@
-import { AtributosService } from '../../../../Shared/servicos/atributos.service';
-import { Component } from '@angular/core';
+import {AtributosService} from '../../../../Shared/servicos/atributos.service';
+import {Component} from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {AtaquesService} from "../../../../Shared/servicos/ataques.service";
 import {MatButton} from "@angular/material/button";
@@ -17,35 +17,49 @@ export class AcoesComponent {
   panelOpenState: boolean = false;
 
   adicionarAtaque() {
-    console.log('fds')
-    let inputAtaque = (<HTMLInputElement>document.getElementById("input-ataque")).value
-    let atributo = (<HTMLSelectElement>document.getElementById("atributo-ataque")).value
-    let proficiencia = (<HTMLSelectElement>document.getElementById("grau-ataque")).value
-    let inputDano = (<HTMLInputElement>document.getElementById("input-dano")).value
-    let inputTipo = (<HTMLInputElement>document.getElementById("input-tipo")).value
-    let inputQntdChar = inputDano.split("d")
+
+    let inputAtaque = this.coletarValorInput("input-ataque")
+    let atributo = this.coletarValorInput("atributo-ataque")
+    let proficiencia = this.coletarValorInput("grau-ataque")
+    let inputDano = this.coletarValorInput("input-dano")
+    let inputTipo = this.coletarValorInput("input-tipo")
+
+    let inputQntdChar = this.separarNumberDano(inputDano)
+
     let inputQntd = Number(inputQntdChar[0])
     let inputDado = Number(inputQntdChar[1])
 
     let bonusProficiencia = this.AtributosService.grauTreinamento(proficiencia)
     let valorGrau;
+
     if(bonusProficiencia > 0){
       valorGrau = 1
     } else{
       valorGrau = 0
     }
+
     let bonus = this.AtaquesService.adicionarAoObjeto(inputAtaque, atributo, bonusProficiencia, inputQntd, inputDado, inputTipo, valorGrau)
 
-    let elementoPai = document.getElementById("ataques")
-    let divElemento = document.createElement('div')
-    let elementoNome = document.createElement('button')
-    elementoNome.classList.add("ataque", "ff-inter")
-    let textoNome = document.createTextNode(inputAtaque)
-    elementoNome.appendChild(textoNome)
-    divElemento.appendChild(elementoNome)
+    this.adicionarAtaqueHTML(inputAtaque, bonus, inputDano, inputTipo)
+    this.panelOpenState = false;
+  }
 
+  coletarValorInput(inputTexto: string){
+    return (<HTMLInputElement>document.getElementById(inputTexto)).value;
+  }
+
+  separarNumberDano(dado: string){
+    return dado.split('d')
+  }
+
+  adicionarAtaqueHTML(inputAtaque: string, bonus: number, inputDano: string, inputTipo: string){
+
+    let elementoPai = document.getElementById("ataques")
+
+    let elementoDiv = document.createElement('div')
+    let ataqueElemento = document.createElement('button')
     let elementoBonus = document.createElement('button')
-    elementoBonus.classList.add("bonus", "ff-inter")
+    let elementoDano = document.createElement('button')
 
     let bonusTextoString = String(bonus)
     let bonusTexto
@@ -56,12 +70,6 @@ export class AcoesComponent {
       bonusTexto = document.createTextNode(bonusTextoString)
     }
 
-    elementoBonus.appendChild(bonusTexto)
-    divElemento.appendChild(elementoBonus)
-
-    let elementoDano = document.createElement('button')
-    elementoDano.classList.add("dano", "ff-inter")
-
     let textoDano
 
     if (inputDano === ""){
@@ -70,12 +78,24 @@ export class AcoesComponent {
       textoDano = document.createTextNode( inputDano + "/" + inputTipo)
     }
 
-    elementoDano.appendChild(textoDano)
-    divElemento.classList.add("ataques-div")
-    divElemento.appendChild(elementoDano)
-    elementoPai?.appendChild(divElemento)
+    ataqueElemento.classList.add("ataque", "vt323", "borda-4px-ataques")
+    elementoBonus.classList.add("bonus", "vt323", "borda-4px-ataques")
+    elementoDano.classList.add("dano", "vt323", "borda-4px-ataques")
 
-    this.panelOpenState = false;
+    let textoNomeAtaque = document.createTextNode(inputAtaque)
+
+    ataqueElemento.appendChild(textoNomeAtaque)
+    elementoDiv.appendChild(ataqueElemento)
+    elementoBonus.appendChild(bonusTexto)
+    elementoDiv.appendChild(elementoBonus)
+    elementoDano.appendChild(textoDano)
+
+    elementoDiv.classList.add("ataques-div")
+
+    elementoDiv.appendChild(elementoDano)
+
+    elementoPai?.appendChild(elementoDiv)
+
   }
 
 
